01507f3502c1c6655875782beea3259971e11432
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2025-03-26 16:58:56 +0530

    cleaning job logic added
    Signed-off-by: Vishal Das <26341736+dvishal485@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
+use chrono::NaiveDateTime;
 use clap::{CommandFactory, Parser};
 use clap_complete::Shell;
+use derive_builder::Builder;
 use log;
 use std::{
-    io,
     path::PathBuf,
     process::{Command, ExitCode},
 };
 use thiserror::Error;
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 struct Args {
     /// Mount point of btrfs filesystem
     #[clap(long, short)]
@@ -25,6 +26,10 @@ struct Args {
     /// Make snapshot readonly
     #[clap(long, short)]
     readonly: bool,
+
+    /// Specify to limit the number of snapshots to keep
+    #[clap(long, short)]
+    count: Option<usize>,
 
     /// Prefix for snapshot name
     #[clap(long)]
@@ -44,57 +49,129 @@ struct Args {
 }
 
 #[derive(Error, Debug)]
-enum ApplicationError<'a> {
+enum ApplicationError {
     #[error("The given mount point \"{0}\" is not a directory")]
-    MountPointNotDir(&'a PathBuf),
+    MountPointNotDir(PathBuf),
     #[error("Failed to run btrfs command")]
-    FailedToSpawnCmd(io::Error),
+    FailedToSpawnCmd(std::io::Error),
     #[error("Failed to query the given subvolume")]
     SubvolumeError,
     #[error("Could not infer snapshot prefix, specify it with --prefix <PREFIX>")]
     PrefixInferenceFailed,
+    #[error("Failed to parse subvolume information\n{0}")]
+    SubvolumeInfoParseFailed(SubvolumeBuilderError),
+    #[error("Failed to delete older subvolume")]
+    SubvolumeDeletionFailed,
 }
 
-fn verify_path(args: &Args) -> Result<(), ApplicationError> {
+fn verify_path(args: &Args) -> Result<Subvolume, ApplicationError> {
+    log::debug!("Verifying mount point");
     if !args.mount_point.is_dir() {
-        return Err(ApplicationError::MountPointNotDir(&args.mount_point));
+        return Err(ApplicationError::MountPointNotDir(args.mount_point.clone()));
     }
-    verify_subvol(args)?;
-    Ok(())
+
+    log::debug!("Fetching subvolume properties");
+    get_subvol(&args.subvol_path)
 }
 
-fn btrfs_show(path: &PathBuf) -> Result<std::process::Output, ApplicationError> {
-    std::process::Command::new("btrfs")
+fn get_subvol(subvol_path: &PathBuf) -> Result<Subvolume, ApplicationError> {
+    let subvol_show = Command::new("btrfs")
         .arg("subvolume")
         .arg("show")
-        .arg(path)
+        .arg(subvol_path)
         .output()
-        .map_err(|e| ApplicationError::FailedToSpawnCmd(e))
-}
+        .map_err(|e| ApplicationError::FailedToSpawnCmd(e))?;
 
-fn verify_subvol(args: &Args) -> Result<(), ApplicationError> {
-    log::debug!("Verifying subvolume properties");
-
-    let subvol_show = btrfs_show(&args.subvol_path)?;
-
-    if args.verbose {
-        let stdout = String::from_utf8_lossy(&subvol_show.stdout);
-        if !stdout.is_empty() {
-            log::info!("{}", stdout.trim());
-        };
+    let stdout = String::from_utf8_lossy(&subvol_show.stdout);
+    if !stdout.is_empty() {
+        log::info!("{}", stdout.trim());
     };
 
     if !subvol_show.status.success() {
-        if args.verbose {
-            let stderr = String::from_utf8_lossy(&subvol_show.stderr);
-            if !stderr.is_empty() {
-                log::error!("{}", stderr.trim());
-            };
+        let stderr = String::from_utf8_lossy(&subvol_show.stderr);
+        if !stderr.is_empty() {
+            log::error!("{}", stderr.trim());
         }
         return Err(ApplicationError::SubvolumeError);
     }
 
-    Ok(())
+    let subvol_info = SubvolumeInfo(stdout);
+
+    let subvol = Subvolume::try_from(subvol_info);
+    subvol
+}
+
+struct SubvolumeInfo<'a>(std::borrow::Cow<'a, str>);
+
+#[derive(Builder)]
+#[builder(pattern = "owned")]
+struct Subvolume {
+    name: String,
+    uuid: String,
+    snapshots: Vec<PathBuf>,
+    creation_time: chrono::NaiveDateTime,
+}
+
+impl<'a> TryFrom<SubvolumeInfo<'a>> for Subvolume {
+    type Error = ApplicationError;
+
+    fn try_from(SubvolumeInfo(info): SubvolumeInfo<'a>) -> Result<Self, Self::Error> {
+        let mut subvol = SubvolumeBuilder::create_empty();
+        let mut snapshots = vec![];
+        let mut snapshot_capture = false;
+        for data in info.lines() {
+            let data = data.trim();
+            if let Some((property, value)) = data.split_once(':') {
+                match property.trim() {
+                    "Name" => subvol = subvol.name(value.trim().to_string()),
+                    "UUID" => subvol = subvol.uuid(value.trim().to_string()),
+                    "Creation time" => {
+                        subvol = subvol.creation_time({
+                            let datetime_str = value.trim();
+                            let subvol_info_time =
+                                NaiveDateTime::parse_from_str(datetime_str, "%Y-%m-%d %H:%M:%S %z");
+                            if let Ok(time) = subvol_info_time {
+                                log::info!("Subvolume creation time: {}", time);
+                                time
+                            } else {
+                                log::warn!(
+                                    "Failed to parse subvolume creation time: {}",
+                                    datetime_str
+                                );
+                                todo!("Impl mech to parse from given format")
+                            }
+                        })
+                    }
+                    property => {
+                        if property.contains("Snapshot") {
+                            log::debug!("Snapshot column encountered!");
+                            snapshot_capture = true;
+                        } else if snapshot_capture {
+                            snapshot_capture = false;
+                            log::debug!("Snapshot column supposedly ended")
+                        }
+                    }
+                }
+            } else {
+                if snapshot_capture {
+                    let snapshot_name = data.trim();
+                    log::info!("Found snapshot {}", snapshot_name);
+                    snapshots.push(snapshot_name);
+                }
+            }
+        }
+
+        subvol = subvol.snapshots(
+            snapshots
+                .into_iter()
+                .map(|snap| PathBuf::from(snap))
+                .collect(),
+        );
+
+        subvol
+            .build()
+            .map_err(|e| ApplicationError::SubvolumeInfoParseFailed(e))
+    }
 }
 
 fn infer_prefix(args: &Args) -> Result<PathBuf, ApplicationError> {
@@ -131,7 +208,12 @@ fn main() -> ExitCode {
     let args = Args::parse();
     if let Some(shell) = args.shell_completion {
         let cmd = &mut Args::command();
-        clap_complete::generate(shell, cmd, cmd.get_name().to_string(), &mut io::stdout());
+        clap_complete::generate(
+            shell,
+            cmd,
+            cmd.get_name().to_string(),
+            &mut std::io::stdout(),
+        );
         return ExitCode::SUCCESS;
     }
 
@@ -180,9 +262,23 @@ fn main() -> ExitCode {
         }
     };
 
-    if let Err(e) = verify_path(&args) {
+    let subvol = verify_path(&args);
+    if let Err(e) = subvol {
         log::error!("{}", e);
         return ExitCode::FAILURE;
+    }
+    let subvol = subvol.unwrap();
+
+    log::debug!(
+        "The specified subvolume {} with UUID {} created on {} has {} snapshots",
+        subvol.name,
+        subvol.uuid,
+        subvol.creation_time,
+        subvol.snapshots.len()
+    );
+
+    if subvol.snapshots.len() > 0 {
+        log::info!("Subvolume snapshots: {:?}", subvol.snapshots);
     }
 
     let curr_time = chrono::Local::now();
@@ -215,7 +311,64 @@ fn main() -> ExitCode {
         return ExitCode::FAILURE;
     };
 
+    log::debug!("Initiating removal of old snapshots");
+
+    let snapshots: Vec<_> = subvol
+        .snapshots
+        .iter()
+        .map(|s| args.mount_point.join(s))
+        .filter(|s| s.starts_with(&args.snapshot_path))
+        .filter_map(|s| get_subvol(&s).ok().map(|subvol| (s, subvol)))
+        .collect();
+
+    if let Some(keep) = args.count {
+        if snapshots.len() > keep {
+            if let Err(e) = cleaning_job(snapshots, keep) {
+                log::error!("{}", e);
+                return ExitCode::FAILURE;
+            }
+        } else {
+            log::info!("No snapshots to remove, count is less than total snapshots");
+        }
+    }
+
     ExitCode::SUCCESS
+}
+
+fn cleaning_job(
+    mut snapshots: Vec<(PathBuf, Subvolume)>,
+    limit: usize,
+) -> Result<(), ApplicationError> {
+    log::debug!("Initiating clean job");
+
+    snapshots.sort_by_key(|(_, s)| std::cmp::Reverse(s.creation_time));
+    for (path, s) in snapshots.drain(limit..) {
+        log::info!("Removing snapshot {:?}", s.name);
+        let output = Command::new("btrfs")
+            .arg("subvolume")
+            .arg("delete")
+            .arg(path)
+            .output()
+            .map_err(|e| ApplicationError::FailedToSpawnCmd(e))?;
+
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        if !stdout.is_empty() {
+            log::info!("{}", stdout.trim());
+        };
+
+        if !output.status.success() {
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            if !stderr.is_empty() {
+                log::error!("{}", stderr.trim());
+            };
+            log::error!("Clean job failed for snapshot {}", s.name);
+            return Err(ApplicationError::SubvolumeDeletionFailed);
+        } else {
+            log::info!("Successfully removed snapshot {}", s.name);
+        }
+    }
+
+    Ok(())
 }
 
 fn btrfs_snapshot(args: &Args, snapshot_file: PathBuf) -> Result<(), ApplicationError> {
