f6f0b353b68748b3475a8f1e0ce878cc379d09ac
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2025-03-29 11:02:58 +0530

    logically separate flake inspired by nh
    Signed-off-by: Vishal Das <26341736+dvishal485@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -3,42 +3,25 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      pname = "btrfs-auto-snapshot";
-    in {
-      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
-        inherit pname;
-        version = "0.1.0";
-        src = ./.;
-
-        useFetchCargoVendor = true;
-        cargoLock = {
-           lockFile = ./Cargo.lock;
-         };
-
-        nativeBuildInputs = [
-          pkgs.installShellFiles
-        ];
-
-        buildInputs = [ pkgs.btrfs-progs ];
-
-        preFixup = ''
-          mkdir completions
-          $out/bin/${pname} completion bash > completions/${pname}.bash
-          $out/bin/${pname} completion zsh > completions/${pname}.zsh
-          $out/bin/${pname} completion fish > completions/${pname}.fish
-
-          installShellCompletion completions/*
-        '';
-
-        meta = {
-          description = "Create and manage Btrfs snapshots automatically";
-          license = pkgs.lib.licenses.mit;
-          maintainers = with pkgs.lib.maintainers; [ imsick ];
-        };
+      rev = self.shortRev or self.dirtyShortRev or "dirty";
+      supportedSystems =
+        function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+        ] (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      overlays.default = final: prev: {
+        btrfs-auto-snapshot = final.callPackage ./package.nix { inherit rev; };
       };
+
+      packages = supportedSystems (pkgs: rec {
+        btrfs-auto-snapshot = pkgs.callPackage ./package.nix { inherit rev; };
+        default = btrfs-auto-snapshot;
+      });
     };
 }
---
+++ package.nix
@@ -0,0 +1,53 @@
+{
+  lib,
+  rustPlatform,
+  installShellFiles,
+  btrfs-progs,
+  rev ? "dirty",
+}:
+
+let
+  cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+  pname = "${cargoToml.package.name}";
+  version = "${cargoToml.package.version}-${rev}";
+in
+rustPlatform.buildRustPackage {
+  inherit pname;
+  inherit version;
+  src = lib.fileset.toSource {
+    root = ./.;
+    fileset = lib.fileset.intersection (lib.fileset.fromSource (lib.sources.cleanSource ./.)) (
+      lib.fileset.unions [
+        ./src
+        ./Cargo.toml
+        ./Cargo.lock
+      ]
+    );
+  };
+
+  useFetchCargoVendor = true;
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+  };
+
+  nativeBuildInputs = [
+    installShellFiles
+  ];
+
+  buildInputs = [ btrfs-progs ];
+
+  preFixup = ''
+    mkdir completions
+    $out/bin/${pname} completion bash > completions/${pname}.bash
+    $out/bin/${pname} completion zsh > completions/${pname}.zsh
+    $out/bin/${pname} completion fish > completions/${pname}.fish
+
+    installShellCompletion completions/*
+  '';
+
+  meta = {
+    description = "Create and manage Btrfs snapshots automatically";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ imsick ];
+  };
+}
